3d05b1f42cf11674f5c821078182d877fb113a38
henri willems <hwillems@wayfair.com>
2024-05-30 11:57:54 +0200

    Convection diffusion example

---
+++ src/convection_diffusion.py
@@ -0,0 +1,76 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from np_funm import funm_krylov, funm_krylov_v2
+from jax_funm import funm_krylov_jittable
+from jax import numpy as jnp
+import os
+
+root_path = os.getcwd()
+
+def get_B(n):
+    h = 1 / (n + 1)
+    return 1 / h**2 * scipy.sparse.diags_array([1, -2, 1], offsets=[-1, 0, 1], shape=(n, n))
+
+def get_Cj(n, mu):
+    h = 1 / (n + 1)
+    return 1 / h**2 * scipy.sparse.diags_array([1 + mu, -2, 1 - mu], offsets=[-1, 0, 1], shape=(n, n))
+
+if __name__ == "__main__":
+    n = 15
+    nx, ny, nz = n, n, n
+    h = 1/ (n + 1)
+    N = nx * ny * nz
+    tau1 = 320  # 96
+    tau2 = 320  # 128
+    mu1 = tau1 * h / 2
+    mu2 = tau2 * h / 2
+    In = scipy.sparse.eye(n, n)
+    A = scipy.sparse.kron(In, scipy.sparse.kron(In, get_Cj(n, mu1))) + \
+        scipy.sparse.kron(scipy.sparse.kron(get_B(n), In) + scipy.sparse.kron(In, get_Cj(n, mu2)), In)
+    t = h ** 2
+    phi = 1 + np.sqrt(1 - mu1**2 + 0j) + np.sqrt(1 - mu2**2 + 0j)
+    spectrum_tA_real = [-6 - 2 * np.cos(np.pi * h) * np.real(phi), -6 + 2 * np.cos(np.pi * h) * np.real(phi)]
+    spectrum_tA_im = [-2 * np.cos(np.pi * h) * np.imag(phi), 2 * np.cos(np.pi * h) * np.imag(phi)]
+
+    b = np.ones(N)
+    exact, _, _ = funm_krylov_v2(t * A, b, {"restart_length": int(n ** 2), "num_restarts":1})
+    for restart_length in [2, 4, 6]:
+        num_restarts = 90 // restart_length + 1
+        param = {
+            "restart_length": restart_length,
+            "num_restarts": num_restarts
+        }
+
+        # Calculate the matrix exponential
+        npfs, npeigvals, npupdate_norms = funm_krylov(t * A.toarray(), b, param)
+        #npfs = funm_krylov_jittable(jnp.array(t * A.toarray()), b, param)
+        npnorms = list(np.linalg.norm(exact - npfs, axis=0))
+        plt.plot(restart_length * np.arange(1, param["num_restarts"] + 1), npnorms, label=f"m:{restart_length}")
+        #plt.scatter(np.arange(1, param["num_restarts"] + 1), npupdate_norms, label=f"m:{restart_length}")
+    plt.title(f"Convection - Diffusion example with n={n} and \mu_1={mu1}, \mu_2={mu2}")
+    plt.legend(framealpha=.5)
+    plt.yscale("log")
+    plt.ylim(bottom=max(np.finfo(npfs.dtype).eps, plt.ylim()[0]) / 10)
+    plt.xlabel("Arnoldi iterations")
+    plt.savefig(os.path.join(root_path, f"figures/convDiff{tau1}ErrorPlot.png"))
+    plt.show()
+
+    plt.title(f"Ritz values for Convection - Diffusion example with n={n} and \mu_1={mu1}, \mu_2={mu2}")
+    ax = plt.gca()
+    rect = patches.Rectangle((spectrum_tA_real[0], spectrum_tA_im[0]),
+                             spectrum_tA_real[1] - spectrum_tA_real[0],
+                             spectrum_tA_im[1] - spectrum_tA_im[0],
+                             linewidth=1, edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
+    # plt.scatter(np.real(sceigvals), np.imag(sceigvals), marker="x", label="scipy", s=10)
+    for k in [param["num_restarts"] - 1]:
+        plt.scatter(np.real(npeigvals[k]), np.imag(npeigvals[k]), marker="x", label=(k + 1) * restart_length, s=20 - k)
+
+    plt.legend()
+    plt.ylabel("Imaginary")
+    plt.xlabel("Real")
+    plt.savefig(os.path.join(root_path, f"figures/convDiff{tau1}RitzValues.png"))
+    plt.show()
+    1 + 1
--- src/jax_funm.py
+++ src/jax_funm.py
@@ -1,8 +1,10 @@
-import jax
-from jax import numpy as jnp
-from jax_Arnoldi import extend_arnoldi, arnoldi_jittable
-import scipy
 from functools import partial
+
+import jax
+import scipy
+from jax import numpy as jnp
+
+from jax_Arnoldi import extend_arnoldi, arnoldi_jittable
 
 
 def funm_krylov(A, b: jax.Array, param):
@@ -34,19 +36,31 @@ def funm_krylov(A, b: jax.Array, param):
     return fs, update_norms
 
 
-@partial(jax.jit, static_argnames=["m", "beta"], device=jax.devices('cpu')[0])
-def calculate_loop(A: jax.Array, H_full: jax.Array, f: jax.Array, w: jax.Array, V_big: jax.Array, m: int, k: int,
-                   beta: float):
+@partial(jax.jit, static_argnames=["m", ])
+def calculate_loop_p1(A, w, m, V_big, H_full, k):
     (w, new_V_big, H) = arnoldi_jittable(A=A, w=w, m=m)
     V_big = jnp.concat([V_big, new_V_big], axis=1)
-    #H_full = H_full.at[k * m: (k + 1) * m, k * m: (k + 1) * m].set(H)
-    #H_full = H_full.at[(k + 1) * m, (k + 1) * m - 1].set(h)
-    jax.lax.dynamic_update_slice(H_full, H, (k * m, k * m))
+    H_full = jax.lax.dynamic_update_slice(H_full, H, (k * m, k * m))
+    return V_big, w, H_full
 
-    H_exp = jax.scipy.linalg.expm(H_full[: (k + 1) * m, : (k + 1) * m])
-    H_exp_jax = jnp.array(H_exp)[-m:, 0]
-    f = beta * (V_big[:, k * m: (k + 1) * m] @ H_exp_jax) + f
+
+@partial(jax.jit, static_argnames=["m", "beta"])
+def calculate_loop_p2(H_exp, beta, V_big, k, m, f):
+    V_slice = jax.lax.dynamic_slice(V_big, (0, k * m), (V_big.shape[0], m))
+    f = beta * (V_slice @ H_exp) + f
     return f
+
+
+# @partial(jax.jit, static_argnames=["m", "beta"], device=jax.devices('cpu')[0])
+def calculate_loop(A: jax.Array, H_full: jax.Array, f: jax.Array, w: jax.Array, V_big: jax.Array, m: int, k: int,
+                   beta: float):
+    V_big, w, H_full = calculate_loop_p1(A, w, m, V_big, H_full, k)
+
+    # H_exp = jax.scipy.linalg.expm(H_full[: (k + 1) * m, : (k + 1) * m])
+    H_slice = jax.lax.dynamic_slice(H_full, (0, 0), ((k + 1) * m, (k + 1) * m))
+    H_exp = jax.scipy.linalg.expm(H_slice)[-m:, 0]
+    f = calculate_loop_p2(H_exp, beta, V_big, k, m, f)
+    return f, H_full, w, V_big
 
 
 def funm_krylov_jittable(A, b: jax.Array, param):
@@ -55,14 +69,13 @@ def funm_krylov_jittable(A, b: jax.Array, param):
     beta = float(jnp.linalg.norm(b))
     w = b
     m = param["restart_length"]
-    V_big = jnp.zeros((n, 0), b.dtype)
+    V_big = jnp.zeros((n, 0))
     f = jnp.zeros_like(b)
-    H_full = jnp.zeros((m * param["num_restarts"] + 1, m * param["num_restarts"]), dtype=b.dtype)
+    H_full = jnp.zeros((m * param["num_restarts"] + 1, m * param["num_restarts"]))
     fs = jnp.zeros((n, param["num_restarts"]))
     update_norms = []
     for k in range(param["num_restarts"]):
-        f = calculate_loop(A, H_full, f, w, V_big, m, k, beta)
-        fs = fs.at[:, k].set(f)
-        #update_norms.append(jnp.linalg.norm(beta * H_exp_jax))
+        f, H_full, w, V_big = calculate_loop(A, H_full, f, w, V_big, m, k, beta)
+        fs = fs.at[:, k].set(f)  # update_norms.append(jnp.linalg.norm(beta * H_exp_jax))
 
-    return fs, update_norms
+    return fs  # , update_norms

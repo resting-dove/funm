68708406030b1025e25bec7bf44a2d2c49e26511
henri willems <hwillems@wayfair.com>
2024-05-30 12:14:24 +0200

    Skew symmetric example

--- src/skew_symmetric.py
+++ src/skew_symmetric.py
@@ -1,16 +1,21 @@
 
 import scipy
 from np_funm import funm_krylov as np_funm_krylov
-
 import numpy as np
-
 import matplotlib.pyplot as plt
+import os
 
-def get_block(j:int):
+root_path = os.getcwd()
+
+
+def get_block(j: int):
     return np.array([[0, 1],
                      [-1, 0]]) * j / 25
+
+
 def get_skew_symmetric_matrix(j=500):
     return scipy.linalg.block_diag(0, *[get_block(j) for j in range(1, j + 1)])
+
 
 if __name__ == "__main__":
     n = 1001
@@ -36,13 +41,15 @@ if __name__ == "__main__":
     plt.yscale("log")
     plt.ylim(bottom=max(np.finfo(npfs.dtype).eps, plt.ylim()[0]) / 10)
     plt.xlabel("Arnoldi iterations")
+    plt.savefig(os.path.join(root_path, f"figures/skewSym{n}ErrorPlot.png"))
     plt.show()
 
     plt.title("Ritz values of skew symmetric matrix")
-    for k in [0, param["num_restarts"] - 1]:
-        plt.scatter(np.real(npeigvals[k]), np.imag(npeigvals[k]), marker="x", label=k, s=20 - k)
+    for k in [param["num_restarts"] - 1]:
+        plt.scatter(np.real(npeigvals[k]), np.imag(npeigvals[k]), marker="x", label=(k + 1) * restart_length, s=20 - k)
 
     plt.legend()
     plt.ylabel("Imaginary")
     plt.xlabel("Real")
+    plt.savefig(os.path.join(root_path, f"figures/skewSym{n}RitzValues.png"))
     plt.show()

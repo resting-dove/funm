3b93efe2b5ea27cb28ac46d025ad8623abdbfa13
henri willems <hwillems@wayfair.com>
2024-06-24 21:03:45 +0200

    use sparse matrices

--- src/skew_symmetric.py
+++ src/skew_symmetric.py
@@ -1,9 +1,10 @@
 
 import scipy
-from np_funm import funm_krylov as np_funm_krylov
+from np_funm import funm_krylov_v2 as np_funm_krylov
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time
 
 root_path = os.getcwd()
 
@@ -14,7 +15,7 @@ def get_block(j: int):
 
 
 def get_skew_symmetric_matrix(j=500):
-    return scipy.linalg.block_diag(0, *[get_block(j) for j in range(1, j + 1)])
+    return scipy.sparse.block_diag((0, *[get_block(j) for j in range(1, j + 1)]), format="csr")
 
 
 if __name__ == "__main__":
@@ -23,16 +24,18 @@ if __name__ == "__main__":
     b = np.ones(n) / np.linalg.norm(np.ones(n))
 
 
-    exact, _, _ = np_funm_krylov(A, b, {"restart_length": n, "num_restarts": 1})
-    for restart_length in [2, 4, 8, 16]:
-        num_restarts = 60 // restart_length + 1
+    exact, eigvals, _ = np_funm_krylov(A, b, {"restart_length": 1000, "num_restarts": 1})
+    for restart_length in [16, 32, 64]:
+        num_restarts = 200 // restart_length + 1
         param = {
             "restart_length": restart_length,
             "num_restarts": num_restarts
         }
 
         # Calculate the matrix exponential
+        start = time.time()
         npfs, npeigvals, npupdate_norms = np_funm_krylov(A, b, param)
+        print(time.time() - start)
         npnorms = list(np.linalg.norm(exact - npfs, axis=0))
         plt.plot(restart_length * np.arange(1, param["num_restarts"] + 1), npnorms, label=f"m:{restart_length}")
         #plt.scatter(np.arange(1, param["num_restarts"] + 1), npupdate_norms, label=f"m:{restart_length}")
@@ -45,6 +48,7 @@ if __name__ == "__main__":
     plt.show()
 
     plt.title("Ritz values of skew symmetric matrix")
+    plt.scatter(np.real(eigvals[0]), np.imag(eigvals[0]), marker="o", label="unrestarted")
     for k in [param["num_restarts"] - 1]:
         plt.scatter(np.real(npeigvals[k]), np.imag(npeigvals[k]), marker="x", label=(k + 1) * restart_length, s=20 - k)
 

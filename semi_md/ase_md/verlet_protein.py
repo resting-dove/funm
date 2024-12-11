697e4ab1ee45ade17a379e3d1f8027ab8b3d2188
resting-dove <henri2@gmx.de>
2024-12-11 16:05:55 +0100

    Setup simulation of 40fs

--- semi_md/ase_md/verlet_protein.py
+++ semi_md/ase_md/verlet_protein.py
@@ -1,17 +1,23 @@
+import os
+import subprocess
+
 import ase.md
 import ase.units as ase_units
+import numpy as np
+import openmm as mm
 import openmm.app as app
 import openmm.unit as unit
 from nanover.ase.omm_calculator import OpenMMCalculator
 from nanover.omni.ase_omm import ASEOpenMMSimulation
 
-from semi_md.ase_md.unit_helpers import eV2kJ_mol
+from semi_md.ase_md.unit_helpers import eV2kJ_mol, ase_unit_system
 from semi_md.utilities.md_plotting_helpers import make_plots
 from semi_md.utilities.read_protein_simulation import read_protein_simulation
 
+root_path = os.getcwd()
 if __name__ == "__main__":
-    n_steps = 400
-    time_step = 0.1 * unit.femtosecond
+    n_steps = 4000
+    time_step = 0.01 * unit.femtosecond
     ase_time_step = time_step.value_in_unit(unit.femtosecond) * ase_units.fs
     simulation: app.Simulation
     simulation, openff_forcefield, openff_topology = read_protein_simulation(time_step)
@@ -23,13 +29,12 @@ if __name__ == "__main__":
     openmm_calculator = OpenMMCalculator(simulation)
     atoms = openmm_calculator.generate_atoms()
     atoms.calc = openmm_calculator
-    conversion_factor = (1 * (unit.nano * unit.meter) / (unit.pico * unit.second)).value_in_unit(
-        unit.angstrom / (unit.femto * unit.second)) * ase_units.Angstrom / ase_units.fs
-    atoms.set_velocities(simulation.context.getState(getVelocities=True).getVelocities() * conversion_factor)
+    atoms.set_velocities(
+        simulation.context.getState(getVelocities=True).getVelocities().value_in_unit_system(ase_unit_system))
 
     vv = ase.md.VelocityVerlet(
         atoms=atoms,
-        timestep=1 * ase_units.fs,
+        timestep=ase_time_step,
         trajectory="protein.traj")
 
     epots, ekins, etots, temps = [], [], [], []
@@ -37,16 +42,21 @@ if __name__ == "__main__":
 
     def printenergy(a=atoms):  # store a reference to atoms in the definition.
         """Function to print the potential, kinetic and total energy."""
-        epot = a.get_potential_energy() * eV2kJ_mol
-        ekin = a.get_kinetic_energy() * eV2kJ_mol
-        etot = a.get_total_energy() * eV2kJ_mol
-        temp = a.get_temperature()
+        state: mm.State = a._calc.context.getState(getEnergy=True, getForces=True, getVelocities=True)
+        epot = state.getPotentialEnergy().value_in_unit_system(
+            unit.md_unit_system)  # a.get_potential_energy() * eV2kJ_mol
+        ekin = state.getKineticEnergy().value_in_unit_system(unit.md_unit_system)  # a.get_kinetic_energy() * eV2kJ_mol
+        etot = epot + ekin  # a.get_total_energy() * eV2kJ_mol
+        temp = (2 * state.getKineticEnergy() / (
+                len(a) * 3 * unit.BOLTZMANN_CONSTANT_kB) / unit.AVOGADRO_CONSTANT_NA).value_in_unit_system(
+            unit.md_unit_system)  # a.get_temperature()
         epots.append(epot)
         ekins.append(ekin)
         etots.append(etot)
         temps.append(temp)
-        print(f'Epot = {epot:.3f}kJ/mol  Ekin = {ekin:.3f}kJ/mol (T={temp:3.0f}K) '
-              f'Etot = {etot:.3f}kJ/mol')
+        if len(epots) % 10 == 0:
+            print(f'Epot = {epot:.3f}kJ/mol  Ekin = {ekin:.3f}kJ/mol (T={temp:3.0f}K) '
+                  f'Etot = {etot:.3f}kJ/mol')
 
 
     # Now run the dynamics
@@ -55,4 +65,13 @@ if __name__ == "__main__":
     vv.run(n_steps)
 
     steps = range(len(temps))
+    plot_store = {
+        "epots": epots,
+        "ekins": ekins,
+        "etots": etots,
+        "temps": temps,
+        "filename": os.path.basename(__file__),
+        "git_commit": subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip(),
+    }
+    np.savez(os.path.join(root_path, "artifacts", "plot_store" + f"VV_protein"), **plot_store)
     make_plots(steps, epots, ekins, etots, temps, "ASE VV: ")

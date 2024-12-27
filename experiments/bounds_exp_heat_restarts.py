3274ba47d13538b13f73d5849a185e04909add2f
resting-dove <henri2@gmx.de>
2024-12-27 20:57:57 +0100

    Latexify bounds_exp_heat_restarts.py

--- experiments/bounds_exp_heat_restarts.py
+++ experiments/bounds_exp_heat_restarts.py
@@ -128,6 +128,7 @@ if __name__ == "__main__":
         idx = get_index(final_size, krylov_size)
         name = f"m:{krylov_size}"
         plot_store[name + " errors"] = error_norms
+        plot_store[name + " updates"] = npupdate_norms
         plot_store[name + " idx"] = idx
         line, = axs[j].plot(idx, error_norms, label=name, linestyle="solid", c="black")
         line, = axs[j].plot(idx[1:], npupdate_norms, linestyle="none", c="black", marker=markers[j])
@@ -136,13 +137,13 @@ if __name__ == "__main__":
         beta = np.linalg.norm(u0.flatten())
         (v, V, H, m) = arnoldi(t * A, u0.flatten() / beta, krylov_size + 50, trunc=1)
 
-        ms, bounds = restarted_pre(min(evals), max(evals), w=w, m=krylov_size, center=center,
-                                             radius=radius,
-                                             starts=bound_n // krylov_size + 1, f=func_scalar)
-        name = f"rest pre {krylov_size}"
-        plot_store[name + " bounds"] = bounds
-        plot_store[name + " ms"] = ms
-        axs[j].plot(ms, exact_norm * bounds, linestyle="-", c=colors[i], label="CGMM")
+        # ms, bounds = chen_musco(min(evals), max(evals), w=w, m=krylov_size, center=center,
+        #                                      radius=radius,
+        #                                      starts=bound_n // krylov_size + 1, f=func_scalar)
+        # name = f"rest pre {krylov_size}"
+        # plot_store[name + " bounds"] = bounds
+        # plot_store[name + " ms"] = ms
+        # axs[j].plot(ms, exact_norm * bounds, linestyle="-", c=colors[i], label="CGMM")
 
         i += 1
         ms, bounds = restarted_post_no_kappa(t * A, u0.flatten(), H[:krylov_size, :krylov_size], w=w, center=center,
@@ -181,8 +182,8 @@ if __name__ == "__main__":
         axs[j].set_ylabel(fr"Error $||\cdot||_{norm_name}$")
         axs[j].legend(framealpha=.5, scatterpoints=1, numpoints=1)
 
-    # np.savez(os.path.join(root_path, "artifacts", "plot_store" + f"bounds_exp_heat_{n}_restarts_{norm_name}-norm"),
-    #          **plot_store)
+    np.savez(os.path.join(root_path, "artifacts", "plot_store" + f"bounds_exp_heat_{n}_restarts_{norm_name}-norm"),
+             **plot_store)
 
     fig.suptitle(
         rf"Heat equation, $\Lambda(A)\subset[{min(evals):.1f}, {max(evals):.1f}]$, " + r"$A\in\mathbb{"
---
+++ experiments/bounds_exp_heat_restarts_visualize.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from experiments.utils import get_fig_axs, get_fig_ax, Colors, postprocess_style, setup_latex
+
+if __name__ == '__main__':
+    setup_latex()
+    fig, axs = get_fig_axs(2, 1, sharex=True)
+    colors = Colors()
+    markers = ["o", "^"]
+    plot_store = np.load("artifacts/plot_storebounds_exp_heat_25_restarts_A-norm.npz")
+
+    line, = axs[0].plot(plot_store["m:5" + " idx"], plot_store["m:5" + " errors"], label="m:5", linestyle="solid",
+                        c="black")
+    line, = axs[0].plot(plot_store["m:5" + " idx"][1:], plot_store["m:5" + " updates"], linestyle="none", c="black",
+                        marker="^")
+
+    line, = axs[1].plot(plot_store["m:20" + " idx"], plot_store["m:20" + " errors"], label="m:20", linestyle="solid",
+                        c="black")
+    line, = axs[1].plot(plot_store["m:20" + " idx"][1:], plot_store["m:20" + " updates"], linestyle="none", c="black",
+                        marker="^")
+
+    i = 2
+    axs[0].plot(plot_store["rest post nk 5" + " ms"], plot_store["rest post nk 5" + " bounds"], linestyle=":",
+                c=colors[i])
+    axs[1].plot(plot_store["rest post nk 20" + " ms"], plot_store["rest post nk 20" + " bounds"], linestyle=":",
+                c=colors[i])
+
+    i += 1
+    axs[0].plot(plot_store["Afanasjew 1 5" + " ms"], plot_store["Afanasjew 1 5" + " bounds"], label="AEEG 1",
+                linestyle="--", c=colors[i])
+    axs[0].plot(plot_store["Afanasjew 2 5" + " ms"], plot_store["Afanasjew 2 5" + " bounds"], label="AEEG 2",
+                linestyle="--", c=colors[i + 1])
+    axs[1].plot(plot_store["Afanasjew 1 20" + " ms"], plot_store["Afanasjew 1 20" + " bounds"], label="AEEG 1",
+                linestyle="--", c=colors[i])
+    axs[1].plot(plot_store["Afanasjew 2 20" + " ms"], plot_store["Afanasjew 2 20" + " bounds"], label="AEEG 2",
+                linestyle="--", c=colors[i + 1])
+
+    axs[0].set_yscale("log")
+    axs[0].set_ylim(bottom=np.finfo(np.float64).eps / 1000, top=10000)
+    axs[-1].set_xlabel("Lanczos iterations")
+    axs[0].set_ylabel(fr"Error $||\cdot||_A$")
+    axs[0].legend(framealpha=.5, scatterpoints=1, numpoints=1)
+    axs[1].set_yscale("log")
+    axs[1].set_ylim(bottom=np.finfo(np.float64).eps / 1000, top=10000)
+    axs[-1].set_xlabel("Lanczos iterations")
+    axs[1].set_ylabel(fr"Error $||\cdot||_A$")
+    axs[1].legend(framealpha=.5, scatterpoints=1, numpoints=1)
+
+    postprocess_style()
+    fig.tight_layout()
+    fig.savefig("figures/bounds_exp_heat_25_restarts_A-norm")

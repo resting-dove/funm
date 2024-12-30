b0a4c8574d2f03f876d10e57bc392f76d1e15c34
resting-dove <henri2@gmx.de>
2024-12-30 12:17:58 +0100

    cross check computeK with symbolic computation

--- semi_md/computeK.py
+++ semi_md/computeK.py
@@ -27,12 +27,12 @@ def compute_K(positions: np.array, bonds: dict, openff_topology: Topology, units
                     .value_in_unit_system(units)
                 difference = positions[i] - positions[j]
                 distance = np.linalg.norm(difference)
-                K_small = k * distance ** (-3) * (- r0) \
+                K_small = k * distance ** (-2) \
+                          * (-1 + (distance - r0) / distance) \
                           * np.outer(difference, difference)
-                add_for_diag = k * -(distance - r0) * distance ** (-1)
+                add_for_diag = k * -(distance - r0) / distance
                 K_small += np.diag(np.ones(3) * add_for_diag)
                 K[i * 3: (i + 1) * 3, j * 3: (j + 1) * 3] = K_small
-                # import pdb; pdb.set_trace()
                 K[j * 3: (j + 1) * 3, j * 3: (j + 1) * 3] -= K_small / 2
                 K[i * 3: (i + 1) * 3, i * 3: (i + 1) * 3] -= K_small / 2  # / 2 because of later + transpose
     K = K + K.transpose()
@@ -58,9 +58,10 @@ def compute_K_with_dict(positions: np.array, bonds_dict: dict, units: UnitSystem
             .value_in_unit_system(units)
         difference = positions[i] - positions[j]
         distance = np.linalg.norm(difference)
-        K_small = k * distance ** (-3) * (- r0) \
+        K_small = k * distance ** (-2) \
+                  * (-1 + (distance - r0) / distance) \
                   * np.outer(difference, difference)
-        add_for_diag = k * -(distance - r0) * distance ** (-1)
+        add_for_diag = k * -(distance - r0) / distance
         K_small += np.diag(np.ones(3) * add_for_diag)
 
         # K[i * 3: (i + 1) * 3, j * 3: (j + 1) * 3] = K_small
@@ -106,9 +107,10 @@ def compute_K_with_force(positions: np.array, force: openmm.HarmonicBondForce, u
         r0 = r0.value_in_unit_system(units)
         difference = positions[i] - positions[j]
         distance = np.linalg.norm(difference)
-        K_small = k * distance ** (-3) * (- r0) \
+        K_small = k * distance ** (-2) \
+                  * (-1 + (distance - r0) / distance) \
                   * np.outer(difference, difference)
-        add_for_diag = k * -(distance - r0) * distance ** (-1)
+        add_for_diag = k * -(distance - r0) / distance
         K_small += np.diag(np.ones(3) * add_for_diag)
 
         # K[i * 3: (i + 1) * 3, j * 3: (j + 1) * 3] = K_small

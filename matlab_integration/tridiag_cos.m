e72b0b77732eb1ea5749a7c230b46ca94316bbbe
resting-dove <henri2@gmx.de>
2025-01-02 18:40:09 +0100

    we no longer need the matlab integration

--- matlab_integration/tridiag_cos.m
+++
@@ -1,7 +0,0 @@
-function C = tridiag_cos(l, d, u)
-    n = length(d);
-    Bin = [[l'; 0], d', [0; u']];
-    A = spdiags(Bin, [-1, 0, 1], n, n);
-    % wkm evaluates COSH(SQRT(A)) and SINHC(SQRT(A)),
-    % further there are the identities SQRT(-A) = i SQRT(A) and COSH(i*A) = COS(A).
-    C = wkm(-A);

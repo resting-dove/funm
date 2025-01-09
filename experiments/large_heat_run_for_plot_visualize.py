9ab3bcff0b4731ad0754a288ff2255f160e61a02
resting-dove <henri2@gmx.de>
2025-01-09 19:10:53 +0100

    remove y-axis label

--- experiments/large_heat_run_for_plot_visualize.py
+++ experiments/large_heat_run_for_plot_visualize.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     ax.set_yscale("log")
     ax.set_ylim(bottom=np.finfo(np.float64).eps / 1000)
     ax.set_xlabel("Lanczos iterations")
-    ax.set_ylabel(r"Error $||\cdot||_A$")
+    # ax.set_ylabel(r"Error $||\cdot||_A$")
     ax.legend(framealpha=.5, scatterpoints=1, numpoints=1)
     postprocess_style()
     fig.tight_layout()
